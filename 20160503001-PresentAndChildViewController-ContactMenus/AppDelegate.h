5c1ed37b66c3282e6f840eb54cc9ac84e8756183
Rainer <it_jinlei.yue@163.com>
2016-05-03 09:10:28 +0800

    Initial Commit

---
+++ 20160503001-PresentAndChildViewController-ContactMenus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160503001-PresentAndChildViewController-ContactMenus
+//
+//  Created by Rainer on 16/5/3.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
